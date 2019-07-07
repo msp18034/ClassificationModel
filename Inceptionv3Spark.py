ba56873796b774eacee1e3654bdf6e22caaa08e4
youngxueya <Youngxueya@gmail.com>
2019-07-07 07:59:51 +0000

    zhahao

--- Inceptionv3Spark.py
+++ Inceptionv3Spark.py
@@ -18,7 +18,10 @@ def parse_record(bytestr):
     example.ParseFromString(bytestr)
     features = example.features.feature
     label = np.array(features['label'].int64_list.value)
-    shape = np.array(features['shape'].int64_list.value)
+    shape = np.array(features['shape'].int64_list.value)[0]
     image = np.array(features['image'].int64_list.value).reshape(shape)
     ingres = np.array(features['ingredients'].int64_list.value)
     return (image, label,ingres)
+
+dataRDD = images.map(lambda x: parse_record(bytes(x[0])))
+
