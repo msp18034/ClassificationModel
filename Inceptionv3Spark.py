18170e7994a94d4073c4604ed59b6b7464af3f0c
YoungXueya <youngxueya@gmail.com>
2019-07-24 15:13:47 +0000

    message

--- Inceptionv3Spark.py
+++ Inceptionv3Spark.py
@@ -9,7 +9,17 @@ import argparse
 import numpy
 import tensorflow as tf
 from datetime import datetime
+from zoo import init_spark_on_yarn
 
+sc = init_spark_on_yarn(
+    hadoop_conf="/opt/hadoop-2.7.5/etc/hadoop",
+    conda_name="base",# The name of the created conda-env
+    num_executor=2,
+    executor_cores=4,
+    executor_memory="2g",
+    driver_memory="2g",
+    driver_cores=2,
+    extra_executor_memory_for_ray="2g")
 from tensorflowonspark import TFCluster
 images = sc.newAPIHadoopFile("hdfs:///shuffled.tfrecord", "org.tensorflow.hadoop.io.TFRecordFileInputFormat", keyClass="org.apache.hadoop.io.BytesWritable",valueClass="org.apache.hadoop.io.NullWritable")
 
@@ -21,7 +31,7 @@ def parse_record(bytestr):
     shape = np.array(features['shape'].int64_list.value)[0]
     image = np.array(features['image'].int64_list.value).reshape(shape)
     ingres = np.array(features['ingredients'].int64_list.value)
-    return (image, label,ingres)
+    return image, [label,ingres]
 
 dataRDD = images.map(lambda x: parse_record(bytes(x[0])))
 
