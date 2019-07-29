e898bc354759e418aa1f299907e78acbee57e380
Yang Xueya <xyyang2@gpugate1.gpufarm.cs.hku.hk>
2019-07-30 00:02:15 +0800

    add testset

--- split.py
+++ split.py
@@ -1,6 +1,56 @@
 import os
 import random
+def split1():
+    valList=[]
+    testList=[]
 
+    count=0
+    with open("all.txt", 'r') as infile:
+        for line in infile:
+            if line is not None:
+                count+=1
+                rnd=random.random()
+                if rnd<0.5:
+                    valList.append(line)
+                else:
+                    testList.append(line)
+
+    val= open("val.txt",'w')
+    for line in valList:
+        val.write(line)
+    test=open("test.txt",'w')
+    for line in testList:
+        test.write(line)
+
+def split():
+    trainList=[]
+    valList=[]
+    testList=[]
+         
+    count=0
+    with open("full.txt", 'r') as infile:
+        for line in infile:
+            if line is not None:
+                count+=1
+                rnd=random.random()
+                if rnd<0.8:
+                    trainList.append(line)
+                elif rnd>=0.8 and rnd<0.95:
+                    valList.append(line)
+                else:
+                    testList.append(line)
+
+    print(len(trainList))
+    train = open("train.txt",'w')
+
+    for line in trainList:
+        train.write(line)
+    val= open("val.txt",'w')
+    for line in valList:
+        val.write(line)
+    test=open("test.txt",'w')
+    for line in testList:
+        test.write(line)
 def split():
     trainList=[]
     valList=[]
@@ -43,4 +93,4 @@ def shuffle():
     for line in lines:
         out.write(line)
 #huffle()
-split()
+split1()
